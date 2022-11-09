f5b606f0d0e33d7d5cec3e94b1a6bb46982a8ea2
trungbach <trunga2k29@gmail.com>
2022-11-09 09:58:40 +0700

    update

--- src/pages/Withdraw/ListWithdraw/TableData/index.js
+++ src/pages/Withdraw/ListWithdraw/TableData/index.js
@@ -161,7 +161,7 @@ function TableData({ dispatch, withdrawStore, pageIndex, setPageIndex }) {
                                                 })}
                                             </div>
                                             {(admin?.role === Role.ROLE_ADMIN ||
-                                                admin?.role === Role.ROLE_ACCOUNTANT) && (
+                                                admin?.role === Role.ROLE_STAFF) && (
                                                 <>
                                                     <img
                                                         onClick={() => handleDeny(item.id)}
