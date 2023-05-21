2c60dcf346fbad1fa000260556a1a744fadae2df
manhtung1999 <tung.accphu.1@gmail.com>
2023-05-21 23:29:52 +0700

    update gitignore

--- .umirc.local.js
+++
@@ -1,7 +0,0 @@
-export default {
-    define: {
-        // API_DOMAIN: 'http://103.7.40.142:6068/',
-        API_DOMAIN: 'http://45.117.170.183:6068/',
-        DOMAIN_NAME: 'http://45.117.170.183:4000',
-    },
-};
